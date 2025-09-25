6c42b882457a877f5a9a788a0f99c27668ae963e
iaguito16 <iagoscastro16@gmail.com>
2025-09-25 13:40:27 -0300

    Js quase pronto, na proxima vez terminar o css e detalhes tecnicos do js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,56 @@
 import chaveApi from "./dadosApi.js"
 
-const url = `http://www.omdbapi.com/?i=tt3896198&apikey=&${chaveApi}`
-
 
 const campoBusca = document.querySelector(`#campoBusca`)
 const btnBusca = document.querySelector(`#pesquisarFilmes`)
-const dadosFilme = document.querySelector (`#dadosFilme`)
+const dadosFilme = document.querySelector(`#dadosFilme`)
 
-/* buscador começa no i depois do ?, ai tem q trocar o i pelo t para buscar os filmes */ 
+async function buscarFilmes() {
+
+    dadosFilme.innerHTML = `Buscando filmes...`
+
+
+    const textoDigitado = campoBusca.value.trim();
+    if (textoDigitado.length === 0) {
+        alert("Por favor, digite o nome de um filme");
+        return;
+    }
+
+    const url = `https://www.omdbapi.com/?s=${textoDigitado}&apikey=${chaveApi}`;
+    try {
+        const resposta = await fetch(url)
+        const data = await resposta.json()
+        console.log(data)
+            mostrarFilmes(data);
+    } catch (erro) {
+        console.log(erro);
+    }
+
+
+}
+
+btnBusca.addEventListener(`click`, buscarFilmes);
+    const textoDigitado = campoBusca.value.trim();
+    buscarFilmes(textoDigitado);
+    campoBusca.value = '';
+
+
+function mostrarFilmes(data) {
+    if (!data.Search) {
+        dadosFilme.innerHTML = 'Nenhum filme encontrado!';
+        return;
+
+    }
+
+    dadosFilme.innerHTML = `<ul>
+        ${data.Search.map(filme => `
+            <li>
+            ${filme.Title}
+            <img src="${filme.Poster}"> 
+            Ano:${filme.Year}</li>
+            `).join('')}
+    </ul>`;
+}
+
+/* buscador começa no i depois do ?, ai tem q trocar o i pelo t para buscar os filmes */
+/*   const url = `https://www.omdbapi.com/?s=${textoDigitado}&apikey=${chaveApi}`;*/
